6f673d978d82bc3cee469ac555ddd1601fcda461
imhilla <hillaryodhiambo282@gmail.com>
2020-11-23 15:32:12 +0300

    add migration

--- db/migrate/20201123120202_create_appointments.rb
+++ db/migrate/20201123120202_create_appointments.rb
@@ -1,6 +1,10 @@
 class CreateAppointments < ActiveRecord::Migration[5.2]
   def change
     create_table :appointments do |t|
+      t.string :username
+      t.string :model
+      t.string :date
+      t.string :city
 
       t.timestamps
     end
