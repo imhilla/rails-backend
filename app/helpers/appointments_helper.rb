d2096c56d78e5ab1e663cee46a265b189107922c
imhilla <hillaryodhiambo282@gmail.com>
2020-11-25 19:20:52 +0300

    add user id params

--- app/helpers/appointments_helper.rb
+++ app/helpers/appointments_helper.rb
@@ -1,5 +1,5 @@
 module AppointmentsHelper
   def appointment_params
-    params.require(:appointment).permit(:username, :model, :date, :city)
+    params.require(:appointment).permit(:username, :model, :date, :city, :user_id)
   end
 end
