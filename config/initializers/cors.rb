379d12b32f9a7aec6c029adc9d11ed289175975d
imhilla <hillaryodhiambo282@gmail.com>
2020-11-27 09:31:04 +0300

    change rack cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origin 'https://infinite-ocean-27248.herokuapp.com'
+    origins 'https://infinite-ocean-27248.herokuapp.com'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 
