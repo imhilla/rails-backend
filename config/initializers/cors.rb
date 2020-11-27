cc13512bc8a1fdd13914a3e0d4bcace405941543
imhilla <hillaryodhiambo282@gmail.com>
2020-11-27 18:26:09 +0300

    fix domain url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,8 +6,4 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 
-  # allow do
-  #   origins '*'
-  #   resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-  # end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
-# if Rails.env == "production"
+if Rails.env == "production"
 Rails.application.config.session_store :cookie_store, key: '_booking_app', domain: 'https://intense-stream-27935.herokuapp.com'
-# else 
-  # Rails.application.config.session_store :cookie_store, key: '_booking_app'
-# end
+else 
+  Rails.application.config.session_store :cookie_store, key: '_booking_app'
+end
