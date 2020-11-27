3a5fa442ba16805f535c32129901dca95472dc03
imhilla <hillaryodhiambo282@gmail.com>
2020-11-27 16:56:22 +0300

    fix origin url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://infinite-ocean-27248.herokuapp.com'
+    origins 'https://intense-stream-27935.herokuapp.com'
     resource '*', 
     headers: :any, 
     methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 # if Rails.env == "production"
-Rails.application.config.session_store :cookie_store, key: '_booking_app', domain: 'infinite-ocean-27248.herokuapp.com'
+Rails.application.config.session_store :cookie_store, key: '_booking_app', domain: 'https://intense-stream-27935.herokuapp.com'
 # else 
   # Rails.application.config.session_store :cookie_store, key: '_booking_app'
 # end
