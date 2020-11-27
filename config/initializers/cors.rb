efd833df2489ffc35fea5efd365cff62eea4e5ec
imhilla <hillaryodhiambo282@gmail.com>
2020-11-27 11:52:50 +0300

    add origins anywhere

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://infinite-ocean-27248.herokuapp.com'
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    origins '*'
+    resource '*', 
+    headers: :any, 
+    methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 
   # allow do
