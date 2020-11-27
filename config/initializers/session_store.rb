75832dc6f83f54b12b8c29d70a5bdeaf870799c5
imhilla <hillaryodhiambo282@gmail.com>
2020-11-27 10:22:30 +0300

    change rack cors

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
-if Rails.env == "production"
+# if Rails.env == "production"
 Rails.application.config.session_store :cookie_store, key: '_booking_app', domain: 'infinite-ocean-27248.herokuapp.com'
-else 
+# else 
   Rails.application.config.session_store :cookie_store, key: '_booking_app'
-end
+# end
