3c125a6687223efc8d66516c8cd6445c722af83e
imhilla <hillaryodhiambo282@gmail.com>
2020-11-28 19:50:31 +0300

    test item model

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do
+    Item.destroy_all
+    Item.create(name: '2020 TESLA MODEL', model: 'WHITE Y', reviews: '4', price: '$65784')
+  end
+  it "is valid with valid attributes" do
+    expect(Item.all.count).to be_eql 1
+  end
 end
