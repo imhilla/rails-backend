8a49373865666fb1eb3a5d32ef1d7d8c07014379
imhilla <hillaryodhiambo282@gmail.com>
2020-11-29 08:45:02 +0300

    make all test pass

--- spec/controllers/appointments_spec.rb
+++ spec/controllers/appointments_spec.rb
@@ -2,14 +2,29 @@ require "rails_helper"
 
 RSpec.describe AppointmentsController, type: :controller do
   describe "post a question route", :type => :request do
+    before(:all) do
+      User.destroy_all
+      User.create(username: 'peter', email: 'peter@gmail.com', password: '$2a$12$bMGfKhJ8AE2M2', id: 1)  
+    end
 
     before do
      post '/appointments', params: { appointment: {username: 'imhilla', model: 'WHITE y', date: '12/23/2020', city: 'Kisumu', user_id: '1'} }
     end
 
-    it 'returns the question' do
+    it 'returns the username' do
       expect(JSON.parse(response.body)['username']).to eq('imhilla')
     end
 
+    it 'returns the model' do
+      expect(JSON.parse(response.body)['model']).to eq('WHITE y')
+    end
+
+    it 'returns the date' do
+      expect(JSON.parse(response.body)['date']).to eq('12/23/2020')
+    end
+
+    it 'returns the city' do
+      expect(JSON.parse(response.body)['city']).to eq('Kisumu')
+    end
   end
 end
