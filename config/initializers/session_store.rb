b2915308eec8806ba130836409754fe65c084657
imhilla <hillaryodhiambo282@gmail.com>
2020-12-01 07:50:19 +0300

    use remote origins

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-Rails.application.config.session_store :cookie_store, key: '_booking_app', domain: 'http://localhost:3000'
+Rails.application.config.session_store :cookie_store, key: '_booking_app', domain: 'https://intense-stream-27935.herokuapp.com/'
 else 
   Rails.application.config.session_store :cookie_store, key: '_booking_app'
 end
