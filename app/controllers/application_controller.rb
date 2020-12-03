2a232fe3699253aada95cd8c8d608af4cc888f71
imhilla <hillaryodhiambo282@gmail.com>
2020-12-03 15:22:45 +0300

    add login routes, and update authentication controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,40 @@
 class ApplicationController < ActionController::Base
-  skip_before_action :verify_authenticity_token
-  before_action :authenticate_request
-  attr_reader :current_api_user
-  
-  private
-  def authenticate_request
-    @current_api_user = AuthorizeApiRequest.call(request.headers)
-                                           .result
-    
-    render json: {  error: "This is not a authorized request." }, 
-                    status: :unauthorized unless @current_api_user
+  # skip_before_action :verify_authenticity_token
+  before_action :authorized
+
+  def encode_token(payload)
+    JWT.encode(payload, 's3cr3t')
+  end
+
+  def auth_header
+    # { Authorization: 'Bearer <token>' }
+    request.headers['Authorization']
+  end
+
+  def decoded_token
+    if auth_header
+      token = auth_header.split(' ')[1]
+      # header: { 'Authorization': 'Bearer <token>' }
+      begin
+        JWT.decode(token, 's3cr3t', true, algorithm: 'HS256')
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
+
+  def logged_in_user
+    if decoded_token
+      user_id = decoded_token[0]['user_id']
+      @user = User.find_by(id: user_id)
+    end
+  end
+
+  def logged_in?
+    !!logged_in_user
+  end
+
+  def authorized
+    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :sessions, only: [:create]
-  resources :registrations, only: [:create]
+  # resources :sessions, only: [:create]
+  # resources :registrations, only: [:create]
   resources :items
+  resources :users
   resources :appointments
+  post '/login', to: 'users#login'
   post 'authenticate', to: 'authentication#authenticate'
   delete :logout, to: "sessions#logout"
   get :logged_in, to: "sessions#logged_in"
