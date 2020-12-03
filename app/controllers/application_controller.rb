ad110e8469f42d8b296e63f3b8d1a5a0711c9b47
imhilla <hillaryodhiambo282@gmail.com>
2020-12-03 12:11:10 +0300

    add authentication controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
+  before_action :authenticate_request
+  attr_reader :current_api_user
+  
+  private
+  def authenticate_request
+    @current_api_user = AuthorizeApiRequest.call(request.headers)
+                                           .result
+    
+    render json: {  error: "This is not a authorized request." }, 
+                    status: :unauthorized unless @current_api_user
+  end
 end
