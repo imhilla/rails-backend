f62349d51acf1d8d51f69fc8b2f7da35a4a219ee
imhilla <hillaryodhiambo282@gmail.com>
2020-12-05 07:03:07 +0300

    unauthorize login fron unregistered users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
       token = encode_token({user_id: @user.id})
       render json: {user: @user, token: token, logged_in: true}
     else
-      render json: {error: "Invalid username or password"}
+      render json: {error: "Invalid username or password"}, status: 401
     end
   end
 
